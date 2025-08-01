b7e1427955651168bf34300861adf628d81a53b6
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-01 16:18:42 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import DataContext from "../datacontext.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
+import { DataProvider } from "../datacontext.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <DataProvider>
