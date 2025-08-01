264e1b7d8cfe8fee94c5975164f63c1181df386d
HasnainShah3881 <hasnainimran3881@gmail.com>
2025-08-01 15:16:33 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { DataProvider } from "../datacontext.jsx";
+import DataContext from "../datacontext.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ToastContainer } from "react-toastify";
-import Profile from "./components/pages/Profile.jsx";
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <DataProvider>
